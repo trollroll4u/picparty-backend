ec0922d901ec22b7f4e263bfd83f6e39200c3145
trollroll4u <yuvalron0@gmail.com>
2024-02-05 20:06:16 +0200

    added events by user

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.use(cors());
+  
   const config = new DocumentBuilder()
     .setTitle('Your API Title')
     .setDescription('Your API Description')
