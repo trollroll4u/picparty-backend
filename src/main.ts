ede726cb8b8f65d08283f43cf095f6b7c3e0f129
trollroll4u <yuvalron0@gmail.com>
2024-02-18 22:23:07 +0200

    add static files

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import * as cors from 'cors';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.use(cors());
+
+  app.useStaticAssets(join(__dirname, '..', 'images'));
   
   const config = new DocumentBuilder()
     .setTitle('Your API Title')
